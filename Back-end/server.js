483c8e44af6966402fa15579e47eb79756be6634
DS Rathore <dsrathorebsw1234@gmail.com>
2021-11-07 18:23:14 +0530

    Changed the path

--- Back-end/server.js
+++ Back-end/server.js
@@ -5,13 +5,12 @@ const PORT = process.env.PORT || 4040;
 
 const app = express();
 app.use(express.static('Public'));
-app.set('view engine', 'ejs')
-app.set('views', path.resolve(__dirname)+ '/Templates/RG-Form')
-
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname)+ '/../Front-end/RG-Form/');
 app.get('/', (req, res) => {
     res.render('sign-up-in.ejs');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening localhost http://localhost:${PORT}`);
-})
+});
